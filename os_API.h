b69bc435c3aedd7eccfcb6b0a622a0fd537861c3
felipe-barria95 <febamo0@gmail.com>
2020-10-12 17:58:23 -0300

    :+1:

--- os_API.h
+++ os_API.h
@@ -1,62 +1,33 @@
 struct File {
+  
 
 } typedef osFile;
 
 
+void os_mount(char* diskname);
 
+void os_bitmap(unsigned num, bool hex);
 
-void os_mount(char* diskname){
+void os_exists(char* path);
 
-};
+void os_ls(char* path);
 
-void os_bitmap(unsigned num, bool hex){
+osFile* os_open(char* path, char mode);
 
-};
+int os_read(osFile* file_desc, void* buffer, int nbytes);
 
-void os_exists(char* path){
+int os_write(osFile* file_desc, void* buffer, int nbytes);
 
-};
+void os_close(osFile* file_desc);
 
-void os_ls(char* path){
+void os_rm(char* path);
 
-};
+int os_hardlink(char* orig, char* dest);
 
-osFile* os_open(char* path, char mode){
+int os_mkdir(char* path);
 
-};
+int os_rmdir(char* path, bool recursive);
 
-int os_read(osFile* file_desc, void* buffer, int nbytes){
+void os_unload(char* orig, char* dest);
 
-};
-
-int os_write(osFile* file_desc, void* buffer, int nbytes){
-
-};
-
-void os_close(osFile* file_desc){
-
-};
-
-void os_rm(char* path){
-
-};
-
-int os_hardlink(char* orig, char* dest){
-
-};
-
-int os_mkdir(char* path){
-
-};
-
-int os_rmdir(char* path, bool recursive){
-
-};
-
-void os_unload(char* orig, char* dest){
-
-};
-
-void os_load(char* orig){
-
-};
+void os_load(char* orig);
