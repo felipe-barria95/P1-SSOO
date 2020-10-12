d706ac2f33f8e79bd26b50e12e1da53f817c20f4
felipe-barria95 <febamo0@gmail.com>
2020-10-12 18:28:53 -0300

    :+1:

--- main.c
+++ main.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
 #include "os_API.h"
+
+FILE file*;
+
+int main(int argc, char **argv){
+  os_mount("nombre_archivo");
+  
+
+  return 0;
+}
--- os_API.c
+++ os_API.c
@@ -1,7 +1,7 @@
 
 
 void os_mount(char* diskname){
-
+  FILE = fopen(diskname, "rb");
 };
 
 void os_bitmap(unsigned num, bool hex){
@@ -9,6 +9,8 @@ void os_bitmap(unsigned num, bool hex){
 };
 
 void os_exists(char* path){
+  unsigned char bytes[255];
+  int n = fread(bytes, 255, 1, fp);
 
 };
 
