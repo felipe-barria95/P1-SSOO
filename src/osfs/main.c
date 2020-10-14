d71539b5010fa9ceb4070ab19902e2debb164a68
Lucas <lzalaquett@uc.cl>
2020-10-14 19:39:08 -0300

    el enunciado actualizado

--- src/osfs/main.c
+++ src/osfs/main.c
@@ -3,7 +3,7 @@
 #include "os_API.h"
 
 int main(int argc, char **argv){
-  os_mount("/Users/felipe/Documents/GitHub/P1-SSOO/disk_files/simdiskfilled.bin");
+  os_mount("./simdiskfilled.bin");
   printf("%i\n", os_exists("/folder"));
   os_ls("/");
   os_bitmap(0, true);
